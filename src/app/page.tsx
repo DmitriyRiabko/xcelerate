0fad8dc9f7656b66424d9678f546a853f538ce2b
DmitriyRiabko <riabko97@gmail.com>
2024-07-23 13:14:27 +0300

    new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import CompIcon from "@/assets/icons/Comp";
 import { Button } from "@/components/ui/Button";
+import { benefitsList } from "@/config";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center min-h-screen overflow-auto bg-mainbg ">
-      <main className="flex flex-col items-center max-w-[1920px] w-full min-h-[100dvh] bg-cover bg-top bg-[url('/images/background.png')] bg-no-repeat">
+      <main className="flex flex-col items-center max-w-[1920px] w-screen min-h-[100dvh] bg-cover bg-top bg-[url('/images/background.png')] bg-no-repeat">
         <div className="p-[150px] pt-[120px]">
           <header className="flex justify-center items-center text-[81px] gap-8 mb-[200px]">
             <img
@@ -16,7 +18,7 @@ export default function Home() {
               <span className="text-primary">-</span> ЦЕ ПРО
             </h1>
 
-            <div className="h-[300px] flex items-end border-b-2 w-[350px]">
+            <div className="h-[300px] flex items-end border-b-2 max-w-[350px] w-full">
               <h1>
                 {"РУХ"}
                 <span className="text-[#F05262]">.</span>
@@ -36,23 +38,41 @@ export default function Home() {
               <span className="text-[#F05262]">?</span>
             </h1>
           </div>
-          <div className="max-w-[55%] text-[24px] mb-[200px]">
+          <div className="w-full lg:max-w-[55%] text-[24px] mb-[120px] leading-7">
             Ми - медіабаїнгова команда, яка за рік свого існування на ринку
             збудувала з нуля всі процеси та, створивши простір з усім необхідним
             для розвитку, масштабування й підкорення нових висот, стрімко вийшла
             на <p>високі показники. </p>
           </div>
-          <div className="flex justify-between w-full items-center">
-            <div className="flex-2 w-full">
-              <Button >Приєднатися</Button>
+          <div className="flex flex-col-reverse justify-between w-full gap-10 items-center mb-[100px] lg:flex-row md">
+            <div className="flex flex-2 w-full justify-center items-center">
+              <Button>Приєднатися</Button>
             </div>
 
-            <p className="flex-3">
+            <div className="flex-3 text-[24px] leading-7">
               Виходити за рамки дозволеного, аналізувати, тестувати. Наша мета -
               підривати індустрію своїми результатами, рухаючись до своїх цілей
               зі швидкістю світла!
-            </p>
+            </div>
           </div>
+          <div className="flex flex-col w-full justify-between lg:flex-row">
+            <div className="flex-1 text-[60px] leading-tight">
+              <h1>
+                Що ти отримаєш, ставши частиною нашого <p>community:</p>
+              </h1>
+            </div>
+            <div className="flex-1">
+              <ul className="mt-[200px]">
+                {benefitsList.map((item) => (
+                  <li className="flex gap-[70px] items-center mb-[70px] justify-start">
+                    <i className="w-5">{item.icon}</i>
+                    <h2 className="text-3xl">{item.text}</h2>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+          <div className="w-full bg-[url('/images/values.png')] h-full max-h-[880px]"></div>
         </div>
       </main>
     </div>
---
+++ src/config.tsx
@@ -0,0 +1,43 @@
+import CompIcon from "./assets/icons/Comp";
+import DollarIcon from "./assets/icons/Dollar";
+import GamesIcon from "./assets/icons/Games";
+import GiftIcon from "./assets/icons/Gift";
+import MerchIcon from "./assets/icons/Merch";
+import MessageIcon from "./assets/icons/Message";
+import StuffIcon from "./assets/icons/Stuff";
+
+interface BenefitItem {
+  text: string;
+  icon: React.ReactNode;
+}
+
+export const benefitsList: BenefitItem[] = [
+  {
+    text: "Вся необхідна технічна інфраструктура",
+    icon: <CompIcon />,
+  },
+  {
+    text: "Високі зарплатні ставки та сітка до 40%",
+    icon: <DollarIcon />,
+  },
+  {
+    text: "Подарунки на важливі дати",
+    icon: <GiftIcon />,
+  },
+  {
+    text: "Корпоративний мерч",
+    icon: <MerchIcon />,
+  },
+  {
+    text: "Крутезна chill area в офісі зі смаколиками, кальяном та PlayStation",
+    icon: <GamesIcon />,
+  },
+  {
+    text: "Корпоративи та класні тім-білдінги",
+    icon: <StuffIcon />,
+  },
+  {
+    text: "Відвідування конференцій",
+    icon: <MessageIcon />,
+  },
+];
