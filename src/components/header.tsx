98cdce9408acd2ef672d5e92be9c9c95b3d4fabd
DmitriyRiabko <riabko97@gmail.com>
2024-07-29 14:54:13 +0300

    fix

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className="flex justify-center items-center   text-[20px] md:text-[40px]   gap-2 lg:gap-8  lg:text-[70px] mb-[10%] xl:mb-[200px] lg:pl-[10%]">
       <img
-        src="/images/logo.png"
+        src="./images/logo.png"
         alt="xcelerate_logo"
         className=" max-h-[30px]  md:max-h-[50px] lg:max-h-[85px] h-full w-auto"
       />
