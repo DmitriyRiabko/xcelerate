3afee9f7ee9146d9978cb7fee480437fd283ac74
DmitriyRiabko <riabko97@gmail.com>
2024-07-29 13:52:02 +0300

    fix №2

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output:'export',
-    assetPrefix:'/xcelerate/',
-    basePath:'/xcelerate'
+    assetPrefix:'/dmitriyriabko.github.io/xcelerate/',
+    basePath:'/dmitriyriabko.github.io/xcelerate'
     
 };
 
